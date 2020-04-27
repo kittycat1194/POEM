7bf2268a39298e39b503e737cdde713b8bf3ca8a
Anastasia <nastenika1194@gmail.com>
2020-04-27 16:22:49 +0300

    Add files via upload

---
+++ Poem.cpp
@@ -0,0 +1,21 @@
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	cout << "\t\"Numbers\"\n\n";
+	cout << "Number \\ONE\\ - this is the sun.\n";
+	cout << "Number \\TWO\\ - this is my shoe.\n";
+	cout << "Number \\THREE\\ - this is a tree.\n";
+	cout << "Number \\FOUR\\ - cats at the door.\n";
+	cout << "Number \\FIVE\\ - I like the pie.\n";
+	cout << "Number \\SIX\\ - this is a stick.\n";
+	cout << "Number \\SEVEN\\ - I see a raven.\n";
+	cout << "Number \\EIGHT\\ - take this plate.\n";
+	cout << "Number \\NINE\\ - I am fine.\n";
+	cout << "Number \\TEN\\ - this is a pen.\n\n";
+	cout << "\t\\\\Author unknown\\\\\n";
+	_getch();
+}
